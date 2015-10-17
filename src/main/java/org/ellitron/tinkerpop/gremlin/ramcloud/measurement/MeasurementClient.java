bfdc2009619ff1c5dd84ce017c8a73d472734e5b
Jonathan Ellithorpe <jde@cs.stanford.edu>
2015-10-17 11:26:24 -0700

    Made MeasurementClient executable by cluster.py
    Added standard RAMCloud client program arguments to MeasurementClient and also
    wrote a launch script for the MeasurementClient to be handed to cluster.py as
    the client argument. This launch script is needed because stock cluster.py
    inserts RAMCloud client arguments at the beginning of the client argument list,
    which for a java program is the classpath and main class to execute, which are
    required to be the first arguments. The launch script simply reverses this
    order.
    
    The goal in doing this is to leverage cluster.py's ability to launch clients on
    multiple servers so as to generate distributed workloads.

--- src/main/java/org/ellitron/tinkerpop/gremlin/ramcloud/measurement/MeasurementClient.java
+++ src/main/java/org/ellitron/tinkerpop/gremlin/ramcloud/measurement/MeasurementClient.java
@@ -36,6 +36,7 @@ import java.util.Collections;
 import java.util.List;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.apache.commons.configuration.BaseConfiguration;
 import org.apache.commons.configuration.Configuration;
 import org.apache.tinkerpop.gremlin.structure.T;
@@ -64,6 +65,9 @@ public class MeasurementClient {
     private static final String CONFIG_TEST_DURATION = "testDuration";
     private static final String CONFIG_RCGRAPH_CONFIG = "ramCloudGraphConfiguration";
     private static final String CONFIG_THREADS = "threads";
+    private static final String CONFIG_NUM_CLIENTS = "numClients";
+    private static final String CONFIG_CLIENT_INDEX = "clientIndex";
+    private static final String CONFIG_LOG_FILE = "logFile";
     
     public static void testAddVertexThroughput(final Configuration testConfig) {
         // Extract test configuration parameters.
@@ -259,17 +263,23 @@ public class MeasurementClient {
      */
     public static void main(String[] args) {
         Options options = new Options();
-        Configuration ramCloudGraphConfig = new BaseConfiguration();
-        Configuration testConfig = new BaseConfiguration();
+        AbstractConfiguration ramCloudGraphConfig = new BaseConfiguration();
+        AbstractConfiguration testConfig = new BaseConfiguration();
         String coordinatorLocator;
-        int totalMasterServers;
+        int numClients;
+        int clientIndex;
+        String logFile;
+        int masterServers;
         long testDuration;
         int threads;
         
         options.addOption("C", "coordinator", true, "Service locator where the coordinator can be contacted.");
-        options.addOption("S", "servers", true, "Total master servers.");
-        options.addOption("D", "duration", true, "Duration of each test.");
-        options.addOption("T", "threads", true, "Number of threads to use.");
+        options.addOption(null, "numClients", true, "Total number of clients.");
+        options.addOption(null, "clientIndex", true, "This client's index number in total clients.");
+        options.addOption(null, "logFile", true, "File to use as the log.");
+        options.addOption(null, "masterServers", true, "Total master servers.");
+        options.addOption(null, "testDuration", true, "Duration of each test.");
+        options.addOption(null, "threads", true, "Number of threads to use.");
         options.addOption("h", "help", false, "Print usage.");
         
         CommandLineParser parser = new DefaultParser();
@@ -287,37 +297,39 @@ public class MeasurementClient {
             return;
         }
         
-        // RAMCloud configuration parameters.
-        if(!cmd.hasOption("C")) {
+        // Required parameters.
+        if(cmd.hasOption("coordinator")) {
+            coordinatorLocator = cmd.getOptionValue("coordinator");
+        } else {
             logger.log(Level.SEVERE, "Missing required argument: coordinator");
             return;
         }
         
-        coordinatorLocator = cmd.getOptionValue("C");
-        
-        if(!cmd.hasOption("S")) {
-            logger.log(Level.SEVERE, "Missing required argument: servers");
+        if(cmd.hasOption("masterServers")) {
+            masterServers = Integer.decode(cmd.getOptionValue("masterServers"));
+        } else {
+            logger.log(Level.SEVERE, "Missing required argument: masterServers");
             return;
         }
         
-        totalMasterServers = Integer.decode(cmd.getOptionValue("S"));
-        
-        ramCloudGraphConfig.setProperty(RAMCloudGraph.CONFIG_COORD_LOC, coordinatorLocator);
-        ramCloudGraphConfig.setProperty(RAMCloudGraph.CONFIG_NUM_MASTER_SERVERS, totalMasterServers);
-        
-        // Test configuration parameters.
-        if(!cmd.hasOption("D")) {
-            logger.log(Level.SEVERE, "Missing required argument: duration");
-            return;
-        }
-        
-        testDuration = Long.decode(cmd.getOptionValue("D"));
-        
+        // Optional parameters.
+        testDuration = Long.decode(cmd.getOptionValue("testDuration", "5"));
         threads = Integer.decode(cmd.getOptionValue(CONFIG_THREADS, "1"));
+        numClients = Integer.decode(cmd.getOptionValue("numClients", "1"));
+        clientIndex = Integer.decode(cmd.getOptionValue("clientIndex", "0"));
+        logFile = cmd.getOptionValue("logFile", null);
         
+        ramCloudGraphConfig.setDelimiterParsingDisabled(true);
+        testConfig.setDelimiterParsingDisabled(true);
+        ramCloudGraphConfig.setProperty(RAMCloudGraph.CONFIG_COORD_LOC, coordinatorLocator);
+        ramCloudGraphConfig.setProperty(RAMCloudGraph.CONFIG_NUM_MASTER_SERVERS, masterServers);
+        
+        testConfig.addProperty(CONFIG_RCGRAPH_CONFIG, ramCloudGraphConfig);
         testConfig.addProperty(CONFIG_TEST_DURATION, testDuration);
         testConfig.addProperty(CONFIG_THREADS, threads);
-        testConfig.addProperty(CONFIG_RCGRAPH_CONFIG, ramCloudGraphConfig);
+        testConfig.addProperty(CONFIG_NUM_CLIENTS, numClients);
+        testConfig.addProperty(CONFIG_CLIENT_INDEX, clientIndex);
+        testConfig.addProperty(CONFIG_LOG_FILE, logFile);
         
         // Run through the tests.
         //testAddVertexThroughput(testConfig);
