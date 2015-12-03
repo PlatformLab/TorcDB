03bff7ed3fbabe89da48b053f6d6c9776800857b
Jonathan Ellithorpe <jde@cs.stanford.edu>
2015-12-02 21:17:27 -0800

    Added config file to setup unit test envars.
    Added a configuration file that sets the proper environment variables required
    to run the unit tests. Currently this includes the coordinator's location and
    the number of servers in the cluster.

---
+++ config/torc-env.sh
@@ -0,0 +1,8 @@
+# This configuration script is used to set the environment variables required
+# by the unit tests.  Please edit this to match your cluster setup.
+
+# The location of your RAMCloud cluster's coordinator.
+#export RAMCLOUD.COORDINATOR_LOCATOR=
+
+# The number of servers in your RAMCloud cluster.
+#export RAMCLOUD.SERVERS=
